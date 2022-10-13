ac8b9e684ad76dd0310c95c0f02da114097a065f
БарашенковИТ <@101-07>
2022-10-13 11:05:36 +0300

    Autorization

--- Monitoring/Authorization.xaml.cs
+++ Monitoring/Authorization.xaml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -31,10 +34,27 @@ namespace Monitoring
             Close();
         }
 
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("Data Source=ngknn.ru;Initial Catalog=Emissions_Barashenkov;Persist Security Info=True;User ID=33П;Password=12357");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            sqlConnection.Close();
+            return dataTable;
+        }
         private void Button_Click_Open(object sender, RoutedEventArgs e)
         {
-            App app = new App();
-            app.connectionString 
+            DataTable dt_user = Select("SELECT * FROM [dbo].[Autorization]"); // получаем данные из таблицы
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+
         }
 
     }
--- Monitoring/Registration.xaml.cs
+++ Monitoring/Registration.xaml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -29,6 +32,28 @@ namespace Monitoring
             Authorization authorization = new Authorization();
             authorization.Show();
             Close();
+        }
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("Data Source=ngknn.ru;Initial Catalog=Emissions_Barashenkov;Persist Security Info=True;User ID=33П;Password=12357");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            sqlConnection.Close();
+            return dataTable;
+        }
+        private void Button_Click_Open(object sender, RoutedEventArgs e)
+        {
+            DataTable dt_user = Select(""); // получаем данные из таблицы
+
+            Authorization authorization = new Authorization();
+            authorization.Show();
+            Close();
+
         }
     }
 }
