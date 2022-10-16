a48f122e76ca131d239cfe6cd9b12a454bf866d2
Илья Барашенков <ya.ilya-03@ya.ru>
2022-10-16 21:03:37 +0300

    DeleteWindow

--- Monitoring/DeleteOutliers.xaml.cs
+++ Monitoring/DeleteOutliers.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Collections;
 
 namespace Monitoring
 {
@@ -19,16 +22,34 @@ namespace Monitoring
     /// </summary>
     public partial class DeleteOutliers : Window
     {
+        public string dele;
         public DeleteOutliers()
         {
             InitializeComponent();
         }
-
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("Data Source=ngknn.ru;Initial Catalog=Emissions_Barashenkov;Persist Security Info=True;User ID=33П;Password=12357");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            sqlConnection.Close();
+            return dataTable;
+        }
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
-            Close();
+            using (SqlConnection sqlConnection = new SqlConnection("server=ngknn.ru;Trusted_Connection=No;DataBase=Emissions_Barashenkov;User ID=33П;Password=12357"))
+            {
+                sqlConnection.Open();
+                dele = IdEmis.Text;
+                int dele1 = Convert.ToInt32(dele);
+                DataTable sqlCommand = Select($"DELETE FROM [dbo].[Emission] WHERE [ID_Emission] = '{dele1}';");
+            }
+                Close();
         }
     }
 }
--- Monitoring/DeleteSource.xaml.cs
+++ Monitoring/DeleteSource.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,7 @@ namespace Monitoring
     /// </summary>
     public partial class DeleteSource : Window
     {
+        public string dele;
         public DeleteSource()
         {
             InitializeComponent();
@@ -26,9 +29,31 @@ namespace Monitoring
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
             Close();
+        }
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("Data Source=ngknn.ru;Initial Catalog=Emissions_Barashenkov;Persist Security Info=True;User ID=33П;Password=12357");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            sqlConnection.Close();
+            return dataTable;
+        }
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection("server=ngknn.ru;Trusted_Connection=No;DataBase=Emissions_Barashenkov;User ID=33П;Password=12357"))
+            {
+                sqlConnection.Open();
+                dele = IdIst.Text;
+                int dele1 = Convert.ToInt32(dele);
+                DataTable sqlCommand = Select($"DELETE FROM [dbo].[Source] WHERE [ID_Source] = '{dele1}';");
+            }
+                Close();
         }
     }
 }
