7aedcc8092c6e21cf2e5fffc128b2a16d9b557cd
Илья Барашенков <ya.ilya-03@ya.ru>
2022-10-16 16:45:51 +0300

    AddEmis

--- Monitoring/AddOutliers.xaml.cs
+++ Monitoring/AddOutliers.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,10 @@ namespace Monitoring
     /// </summary>
     public partial class AddOutliers : Window
     {
+        public string idsource;
+        public string count;
+        public string comment;
+        public string dt;
         public AddOutliers()
         {
             InitializeComponent();
@@ -28,6 +34,28 @@ namespace Monitoring
         {
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
+            Close();
+        }
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("Data Source=ngknn.ru;Initial Catalog=Emissions_Barashenkov;Persist Security Info=True;User ID=33П;Password=12357");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            sqlConnection.Close();
+            return dataTable;
+        }
+        private void Dob_Click(object sender, RoutedEventArgs e)
+        {
+            idsource = IdIst.Text;
+            count = CountEmis.Text;
+            comment = Comm.Text;
+            dt = DateEmis.Text;
+            DataTable addemis = Select("Insert into Emission (ID_Source,count,Text,date) Values ('"+ idsource +"','"+ count +"','"+ comment +"','"+ dt +"');");
             Close();
         }
     }
