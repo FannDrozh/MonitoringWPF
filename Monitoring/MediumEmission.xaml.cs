1ab7a4679b09cd363f7de5a02169a8be54f0acd8
БарашенковИТ <@208-02>
2022-10-17 14:53:12 +0300

    WindowMaxMinMed

---
+++ Monitoring/MediumEmission.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Monitoring
+{
+    /// <summary>
+    /// Логика взаимодействия для MediumEmission.xaml
+    /// </summary>
+    public partial class MediumEmission : Window
+    {
+        public MediumEmission()
+        {
+            InitializeComponent();
+        }
+    }
+}
